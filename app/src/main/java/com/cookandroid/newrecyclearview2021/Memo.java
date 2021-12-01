de1c8cdaf0cb6730be118d50ae8fae4cf2456ce6
bandal0716 <dnjswns505516@gmail.com>
2021-12-01 19:04:59 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/newrecyclearview2021/Memo.java
@@ -0,0 +1,19 @@
+package com.cookandroid.newrecyclearview2021;
+
+public class Memo {
+    String date;
+    String content;
+
+    public Memo(String date, String content){
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getDate() {
+        return  date;
+
+    }
+    public String getContent(){
+        return  content;
+    }
+}
